5b74a7e36e3d21d3153ea615718b0653d3c89f5e
Mikhail <caplin.m@inbox.ru>
2022-08-13 14:45:47 +1000

    add folder Lesson_1

---
+++ lesson_1/task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+//
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+
+Console.WriteLine("Введите два числа!");
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b) 
+{
+    Console.WriteLine($"a = {a}; b = {b} -> max = {a}");
+}
+
+else 
+{
+    Console.WriteLine($"a = {a}; b = {b} -> max = {b}");
+}
---
+++ lesson_1/task_4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+
+Console.WriteLine("Введите три числа!");
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+
+if (a > b && b > c) 
+{
+    Console.WriteLine($"a = {a}; b = {b}; c = {c} -> max = {a}");
+}
+
+else if (a < b && b > c) 
+{
+    Console.WriteLine($"a = {a}; b = {b}; c = {c} -> max = {b}");
+}
+else 
+{
+    Console.WriteLine($"a = {a}; b = {b}; c = {c} -> max = {c}");
+}
