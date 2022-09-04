1872fe4a8a1aa63904b5d40c518aeafb2a519600
Mikhail <caplin.m@inbox.ru>
2022-09-04 16:42:19 +1000

    add folder task_62 - 'Cпиральный массив'

---
+++ Lesson_8/task_62/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+Console.Clear();
+
+
+
+void NewArray(int[,] array)
+{
+    int count = 1;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = count++;
+        }
+
+    }
+}
+
+void PrintArray(int[,] values)
+{
+    for (int i = 0; i < values.GetLength(0); i++)
+    {
+        for (int j = 0; j < values.GetLength(1); j++)
+        {
+
+            Console.Write($"{values[i, j]} ");
+        }
+        Console.WriteLine();
+
+    }
+}
+
+void FillArray(int row, int colum)
+{
+    int[,] array = new int[5, 5];
+    if (array[row, colum] == 0)
+    {
+        FillArray(row, colum + 1);
+        FillArray(row + 1, colum);
+        FillArray(row, colum - 1);
+        FillArray(row - 1, colum);
+    }
+}
+
+
+
+int[,] array = new int[5, 5];
+// Console.WriteLine($"Двумерный массив размером {m} x {n}, заполненный случайными числами.");
+Console.WriteLine();
+
+NewArray(array);
+PrintArray(array);
+FillArray(3,3);
+PrintArray(array);
