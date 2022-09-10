5e442dd4d07589df185d78549b9f34f80080cbc6
Mikhail <caplin.m@inbox.ru>
2022-09-10 14:09:57 +1000

    add folder task_69 - 'Возведенение в степень'

---
+++ Lesson_9/task_69/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+
+// A = 3; B = 5 -> 243
+// A = 2; B = 3 -> 8
+
+Console.Clear();
+
+int Exponentiation(int a, int b)
+{
+    if (a == b)
+    {
+        return a;
+    }
+    return a * a * Exponentiation(a, b - 1);
+}
+
+Console.WriteLine("Введите два числа A и B, для того, чтобы возвести число А в целую степень B.");
+Console.WriteLine();
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int exponentiation = Exponentiation(a, b);
+
+Console.WriteLine();
+Console.WriteLine($"A = {a}; B = {b} -> {exponentiation}");
