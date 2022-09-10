895ab260f15d3c76d1d66935c63f3941edf53f12
Mikhail <caplin.m@inbox.ru>
2022-09-10 11:35:43 +1000

    add folder task_64 - 'Сумма чисел'

---
+++ Lesson_9/task_64/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, 
+// которая найдет сумму чисел от M до N.
+
+// M = 1; N = 5. -> 1, 2, 3, 4, 5
+// M = 4; N = 8. -> 4, 6, 7, 8
+
+Console.Clear();
+
+int SumOfNumbers(int m, int n)
+{
+    if (m == n)
+    {
+        return m;
+    }
+    return n + SumOfNumbers(m, n - 1);
+}
+
+void PrintFromMToN (int m, int n)
+{
+    while (m != n+1)
+    {
+        Console.Write($"{m}, ");
+        m++;
+    }
+    Console.Write("\b");
+    Console.Write("\b");
+}
+
+Console.WriteLine("Введите диапозон чисел от M до N, для нахождения суммы чисел от M до N.");
+Console.WriteLine();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int sumNumber = SumOfNumbers(m, n);
+
+Console.WriteLine();
+Console.Write($"M = {m}; N = {n}. -> "); 
+PrintFromMToN(m, n);
+Console.Write($" Сумма чисел = {sumNumber}");
